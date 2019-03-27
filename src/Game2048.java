c5b150163423a5e681f56f3158ab8fa866d1c80f
nguyenvietngocquang <nguyenvietngocquang@users.noreply.github.com>
2019-03-28 00:56:08 +0700

    Day 1

--- src/Game2048.java
+++ src/Game2048.java
@@ -5,17 +5,52 @@ import java.text.*;
 import java.util.*;
 
 public class Game2048 {
-	private static int board[][] = new int[4][4];
-	JTable table = new JTable();
-	Random rd = new Random();
+	static Scanner sc = new Scanner(System.in);
 	
-	void start() {
-		this.random();
-		this.random();
-		this.random();
+	public static void main(String[] args) {
+		Game2048 game = new Game2048();
+		game.start();
+		print();
+		int c=0;
+		do {
+			c = sc.nextInt();
+			sc.nextLine();
+			switch(c) {
+			case 2: {
+				moveDown();
+				print();
+				break;
+			}
+			case 4: {
+				moveLeft();
+				print();
+				break;
+			}
+			case 6: {
+				moveRight();
+				print();
+				break;
+			}
+			case 8: {
+				moveUp();
+				print();
+				break;
+			}
+			}
+		} while (c!=0);
 	}
 	
-	void random() {
+	static int board[][] = new int[4][4];
+	JTable table = new JTable();
+	static Random rd = new Random();
+	
+	void start() {
+		random();
+		random();
+		random();
+	}
+	
+	static void random() {
 		int x, y;
 		do {
 			x = rd.nextInt(4);
@@ -27,24 +62,243 @@ public class Game2048 {
 		} else board[x][y] = 4;
 	}
 	
-	boolean checkZero(int x, int y) {
+	static boolean checkZero(int x, int y) {
 		if (board[x][y] == 0)
 				return true;
 		else return false;
 	}
 	
-	public void print() {
+	static boolean checkMove() {
+		return false;
+	}
+	
+	public static void print() {
 		for (int i=0; i<4; i++) {
 			for (int j=0; j<4; j++) {
 				System.out.print(board[i][j] + "\t");
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
 	
-	public static void main(String[] args) {
-		Game2048 game = new Game2048();
-		game.start();
-		game.print();
+	static void moveLeft() {
+		boolean check;
+		for (int i=0; i<4; i++) {
+			int k = 0;
+			for (int j=0; j<4; j++) {
+				if (board[i][j] != 0) {
+					board[i][k] = board[i][j];
+					k++;
+				}
+			}
+			for (int j=k; j<4; j++) {
+				board[i][j] = 0;
+			}
+		}
+		do {
+			check = grossLeft();
+		} while (check==false);
+		checkMove();
+		random();
+	}
+	
+	static boolean grossLeft() {
+		for (int i=0; i<4; i++) {
+			int k = 0;
+			for (int j=0; j<4; j++) {
+				if (board[i][j] != 0) {
+					if (j<3) {
+						if (board[i][j] == board[i][j+1]) {
+							board[i][k] = 2*board[i][j];
+							k++;
+							j++;
+						} else {
+							board[i][k] = board[i][j];
+							k++;
+						}
+					} else {
+						board[i][k] = board[i][j];
+						k++;
+					}
+				}
+			}
+			for (int j=k; j<4; j++) {
+				board[i][j] = 0;
+			}
+		}
+		for (int i=0; i<4; i++) {
+			for (int j=0; j<3; j++) {
+				if ((board[i][j]!=0) && (board[i][j] == board[i][j+1])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	static void moveRight() {
+		boolean check;
+		for (int i=0; i<4; i++) {
+			int k = 3;
+			for (int j=3; j>=0; j--) {
+				if (board[i][j] != 0) {
+					board[i][k] = board[i][j];
+					k--;
+				}
+			}
+			for (int j=0; j<=k; j++) {
+				board[i][j] = 0;
+			}
+		}
+		do {
+			check = grossRight();
+		} while (check==false);
+		checkMove();
+		random();
+	}
+	
+	static boolean grossRight() {
+		for (int i=0; i<4; i++) {
+			int k = 3;
+			for (int j=3; j>=0; j--) {
+				if (board[i][j] != 0) {
+					if (j>0) {
+						if (board[i][j] == board[i][j-1]) {
+							board[i][k] = 2*board[i][j];
+							k--;
+							j--;
+						} else {
+							board[i][k] = board[i][j];
+							k--;
+						}
+					} else {
+						board[i][k] = board[i][j];
+						k--;
+					}
+				}
+			}
+			for (int j=0; j<=k; j++) {
+				board[i][j] = 0;
+			}
+		}
+		for (int i=0; i<4; i++) {
+			for (int j=3; j>0; j--) {
+				if ((board[i][j]!=0) && (board[i][j] == board[i][j-1])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	static void moveUp() {
+		boolean check;
+		for (int j=0; j<4; j++) {
+			int k = 0;
+			for (int i=0; i<4; i++) {
+				if (board[i][j] != 0) {
+					board[k][j] = board[i][j];
+					k++;
+				}
+			}
+			for (int i=k; i<4; i++) {
+				board[i][j] = 0;
+			}
+		}
+		do {
+			check = grossUp();
+		} while (check==false);
+		checkMove();
+		random();
+	}
+	
+	static boolean grossUp() {
+		for (int j=0; j<4; j++) {
+			int k = 0;
+			for (int i=0; i<4; i++) {
+				if (board[i][j] != 0) {
+					if (i<3) {
+						if (board[i][j] == board[i+1][j]) {
+							board[k][j] = 2*board[i][j];
+							k++;
+							i++;
+						} else {
+							board[k][j] = board[i][j];
+							k++;
+						}
+					} else {
+						board[k][j] = board[i][j];
+						k++;
+					}
+				}
+			}
+			for (int i=k; i<4; i++) {
+				board[i][j] = 0;
+			}
+		}
+		for (int j=0; j<4; j++) {
+			for (int i=0; i<3; i++) {
+				if ((board[i][j]!=0) && (board[i][j] == board[i+1][j])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	static void moveDown() {
+		boolean check;
+		for (int j=0; j<4; j++) {
+			int k = 3;
+			for (int i=3; i>=0; i--) {
+				if (board[i][j] != 0) {
+					board[k][j] = board[i][j];
+					k--;
+				}
+			}
+			for (int i=0; i<=k; i++) {
+				board[i][j] = 0;
+			}
+		}
+		do {
+			check = grossDown();
+		} while (check==false);
+		checkMove();
+		random();
+	}
+	
+	static boolean grossDown() {
+		for (int j=0; j<4; j++) {
+			int k = 3;
+			for (int i=3; i>=0; i--) {
+				if (board[i][j] != 0) {
+					if (i>0) {
+						if (board[i][j] == board[i-1][j]) {
+							board[k][j] = 2*board[i][j];
+							k--;
+							i--;
+						} else {
+							board[k][j] = board[i][j];
+							k--;
+						}
+					} else {
+						board[k][j] = board[i][j];
+						k--;
+					}
+				}
+			}
+			for (int i=0; i<=k; i++) {
+				board[i][j] = 0;
+			}
+		}
+		for (int j=0; j<4; j++) {
+			for (int i=3; i>0; i--) {
+				if ((board[i][j]!=0) && (board[i][j] == board[i-1][j])) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
